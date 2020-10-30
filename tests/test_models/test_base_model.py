bf52787160d242bf00944b97e62105e88e61a5ac
Athesto <gamez.live@gmail.com>
2020-10-30 01:01:21 -0500

    [update] test_base_model (base_model cases)

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -12,3 +12,29 @@ class TestBaseModel(unittest.TestCase):
         my_model = BaseModel()
         my_model.name = "Holberton"
         self.assertEqual(my_model.name, "Holberton")
+
+    def test_id(self):
+        '''check id'''
+        my_model = BaseModel()
+        self.assertTrue(my_model.id)
+        self.assertEqual(type(my_model.id), str)
+
+    def test_created_at(self):
+        '''check created_at'''
+        # TODO: created from json
+        from datetime import datetime
+        my_model = BaseModel()
+        self.assertEqual(type(my_model.created_at), datetime)
+
+    def test_updated_at(self):
+        '''check updated_at'''
+        # TODO: created from json
+        # TODO: check updated
+        from datetime import datetime
+        my_model = BaseModel()
+        self.assertEqual(type(my_model.updated_at), datetime)
+
+    def test_str(self):
+        '''check __str__ method'''
+        my_model = BaseModel()
+        self.assertTrue(my_model.id)
