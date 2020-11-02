7e8871021ae2526df8a681e556a13f1f72e5b7ee
Athesto <gamez.live@gmail.com>
2020-11-02 12:31:01 -0500

    [updated] added test BaseModel
    separated test to 1 test per function

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -17,7 +17,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(my_model.name, "Holberton")
 
     def test_id_noArgs(self):
-        '''check id w/o args'''
+        '''check type/value of id w/o args'''
         my_model = BaseModel()
         self.assertTrue(my_model.id)
         self.assertEqual(type(my_model.id), str)
@@ -26,16 +26,6 @@ class TestBaseModel(unittest.TestCase):
         '''check type of created_at w/o args'''
         my_model = BaseModel()
         self.assertEqual(type(my_model.created_at), datetime)
-
-    def test_created_at_noArgs_afterSave(self):
-        '''check created_at w/o args after save'''
-        now = datetime.now().replace(microsecond=0)
-        my_model = BaseModel()
-        # test value created_at
-        self.assertEqual(my_model.created_at.replace(microsecond=0), now)
-        # test value changed_no_update
-        my_model.save()
-        self.assertEqual(my_model.created_at.replace(microsecond=0), now)
 
     def test_created_at_noArgs_format(self):
         '''check format %Y-%M-%DT%H:%M:%S.%MS'''
@@ -46,22 +36,36 @@ class TestBaseModel(unittest.TestCase):
         format_found = datetime_format.match(my_created_at)
         self.assertIsNotNone(format_found)
 
-    def test_updated_at(self):
-        '''check updated_at'''
-        # TODO: created from json
-        # TODO: check updated
+    def test_created_at_noArgs_value(self):
+        '''check value of created_at w/o args'''
         now = datetime.now().replace(microsecond=0)
         my_model = BaseModel()
-        # test type(created_at)
-        self.assertEqual(type(my_model.updated_at), datetime)
-        # test value created_at
-        self.assertEqual(my_model.updated_at.replace(microsecond=0), now)
-        # Time skip
-        sleep(1)
-        # test value changed_no_update
+        self.assertEqual(my_model.created_at.replace(microsecond=0), now)
+
+    def test_created_at_noArgs_afterSave(self):
+        '''check created_at w/o args after save()'''
+        my_model = BaseModel()
+        my_created_at = my_model.created_at
         my_model.save()
+        self.assertEqual(my_model.created_at, my_created_at)
+
+    def test_updated_at_noArgs_type(self):
+        '''check type updated_at'''
+        my_model = BaseModel()
+        self.assertEqual(type(my_model.updated_at), datetime)
+
+    def test_updated_at_noArgs_value(self):
+        '''check value of updated_at'''
         now = datetime.now().replace(microsecond=0)
+        my_model = BaseModel()
         self.assertEqual(my_model.updated_at.replace(microsecond=0), now)
+
+    def test_updated_at_noArgs_value_afterSave(self):
+        '''check value of updated_at after save()'''
+        my_model = BaseModel()
+        updated_pre = my_model.updated_at
+        my_model.save()
+        self.assertTrue(my_model.updated_at > updated_pre)
 
     def test_str(self):
         '''check __str__ method'''
@@ -69,3 +73,18 @@ class TestBaseModel(unittest.TestCase):
         r = re.compile("\[BaseModel\] (.*) {.*}")
         my_str = my_model.__str__()
         self.assertIsNotNone(r.match(my_str))
+
+    def test_to_dict_noAditonalArgsAttr(self):
+        '''check to_dict w/o additional args'''
+        my_model = BaseModel()
+        BaseModel.name = "holberton"
+        attributes = {}
+        for key, value in my_model.to_dict().items():
+            if (key not in ('__class__', 'id', 'created_at', 'updated_at')):
+                attributes[key] = value
+        self.assertFalse(attributes)
+
+    def test_to_dict_class(self):
+        '''check to_dict w/o additional args'''
+        my_model = BaseModel()
+        self.assertEqual(my_model.to_dict()['__class__'], 'BaseModel')
