20eeac39726a13c1f88b2299da9cbee9637eae85
Athesto <gamez.live@gmail.com>
2020-11-04 13:16:38 -0500

    [new] tests/test_console.py

---
+++ tests/test_console.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''testing console file'''
+
+import unittest
+
+
+class TestConsole(unittest.TestCase):
+    '''TestConsole class'''
+
+    def test_demo(self):
+        '''demo test'''
+        self.assertTrue(True)
