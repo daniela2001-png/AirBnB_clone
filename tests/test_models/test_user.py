ad3a522db4dfce0f912d84ffb60a971873a3e777
Athesto <gamez.live@gmail.com>
2020-11-04 11:07:58 -0500

    [update] testing User  __str__

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,6 +2,7 @@
 '''test user class'''
 
 import os
+import re
 from models import storage
 from unittest import TestCase
 from models.user import User
@@ -23,6 +24,22 @@ class TestUser(TestCase):
     def test_demo(self):
         '''test demo'''
         self.assertTrue(True)
+
+    def test_format_str(self):
+        '''test __str__'''
+        my_user = User()
+        my_user.first_name = "betty"
+        my_user.first_name = "holberton"
+        my_user.email = "air@holberton.com"
+        my_user.save()
+        my_str = my_user.__str__()
+        r = re.compile(r"\[User\] \(.*\) \{.*\}")
+        self.assertIsNotNone(r.match(my_str))
+
+
+##################
+# Unusefull Test #
+##################
 
     def test_Subclass(self):
         '''user class inherit from BaseModel'''
